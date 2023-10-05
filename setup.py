ba06b0e32fc8bc2a1ca52cdbc6419550174a6c94
Thiago Vinicius <tthiagovinnicius@gmail.com>
2023-10-05 22:46:16 +0000

    Criando o Menu

--- setup.py
+++ setup.py
@@ -1,7 +1,17 @@
 from crud.database import Database
 from utils import aniLoad, Sistema
+import os
 
-aniLoad(time=2, value="Conectando-se ao banco de dados")
-sis = Sistema()
-while True: 
-    sis.menu()
+
+SISTEMA = Sistema()
+
+def main():
+    os.system('clear || cls')
+
+    # Inciando a conexão com o banco.
+    aniLoad(value="Conectando-se ao Banco de dados", time=2)
+    SISTEMA.conectar(dbpath='./database/Empresa.db')
+    SISTEMA.menu()
+    
+if __name__ == '__main__':
+    main()
