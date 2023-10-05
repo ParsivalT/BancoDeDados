eaf04c0aee046e00d6017e38df8c56a5c7bd651c
Thiago Vinicius <tthiagovinnicius@gmail.com>
2023-10-05 01:03:59 +0000

    Menu

--- setup.py
+++ setup.py
@@ -1,4 +1,7 @@
 from crud.database import Database
-from utils import aniLoad, console, menu
+from utils import aniLoad, Sistema
 
 aniLoad(time=2, value="Conectando-se ao banco de dados")
+sis = Sistema()
+while True: 
+    sis.menu()
--- utils.py
+++ utils.py
@@ -1,14 +1,41 @@
 from rich.console import Console
+from rich.panel import Panel
 from time import sleep
 
-console = Console()
+CONS = Console()
 
 # Animação para melhorar a experiencia do usuario.
 def aniLoad(time:int=1, value:str="Carregando"):
-    with console.status(f"[bold green]{value}...") as status:
+    with CONS.status(f"[bold green][b]{value}[/b]...") as status:
         for _ in range(time):
             sleep(1)
 
 # TODO: Criar o menu com as opções para ADICIONAR, DELETAR, ATUALIZAR
-def menu():
-    pass
+
+class Sistema():
+    def __init__(self):
+        pass
+
+
+    def menu(self):
+        CONS.clear()
+        titulo = '[b]CRUD - Python[/b]'
+        opcao = Panel(
+            """
+            Selecione uma opção:
+
+            [b]1[/b] - [cyan b]Listar[/] produtos;
+            [b]2[/b] - [green b]Inserir[/] produtos;
+            [b]3[/b] - [yellow b]Atualizar[/] produtos;
+            [b]4[/b] - [red b]Deletar[/] produtos;
+            [b]5[/b] - [deep_pink4]Sair[/] do programa.
+
+            [grey]Pressione [b]m[/b] para abrir o menu novamente[/]
+        """,title='[b]Gerenciamento de Funcionarios[/b]')
+
+        CONS.rule(title=titulo, align='center')
+        CONS.print(opcao)
+
+        input()
+
+
