c2ac8f274e6f72537615c9064e63fbdffe4ba562
Thiago Vinicius <tthiagovinnicius@gmail.com>
2023-10-19 00:13:17 +0000

    Melhorando os comentarios

--- login/main.py
+++ login/main.py
@@ -1,41 +1,51 @@
+# Importando a biblioteca bcrypt para geração de hashes a partir de senhas informadas pelos usuários.
 import bcrypt
+# Importando a biblioteca sqlite3 para trabalhar com o banco de dados SQLite.
 import sqlite3
+# Importando a função getpass para receber senhas do usuário sem exibi-las na tela.
 import getpass
-from  os import system
+# Importando a função system do módulo os para limpar a tela.
+from os import system
 
-# Conectando-se ao Banco de Dados
+
+# Conectando-se ao Banco de Dados SQLite.
 CONN = sqlite3.connect("database.db")
 CURSOR = CONN.cursor()
 
 # Codigo de apoio para criar a tabela
-'''cursor.execute("""
+CURSOR.execute("""
         CREATE TABLE IF NOT EXISTS Usuarios (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         usuario VARCHAR(60) NOT NULL UNIQUE,
         senha VARCHAR(60) NOT NULL,
         salt VARCHAR(60) NOT NULL
 )
-""")'''
+""")
 
+# Definindo uma função para cadastrar um novo usuário.
 def cadastrar_usuario() -> None:
     print("------CADASTRANDO NOVO USUARIO------")
 
-    # Capturando a entrada do Usuario.
+    # Capturando a entrada do usuário para nome e senha.
     nome:str = input("Insira seu nome: ")
     senha:str = input("Insira sua senha: ")
 
-    # Gerando um salt aleatorio junto de um hash para a senha.
+    # Gerando um salt aleatório junto com um hash para a senha.
+
+    """
+    Salt é uma sequência de dados aleatórios que é gerada e usada em conjunto 
+    com uma senha antes de calcular seu hash.
+    """
     salt:bytes = bcrypt.gensalt(rounds=12)
     senha_hash:bytes = bcrypt.hashpw(password=senha.encode("utf-8"), salt=salt)
 
-    # Salvando os dados no banco.
+    # Salvando os dados no banco de dados.
     CURSOR.execute("""INSERT INTO Usuarios (usuario, senha, salt) VALUES (?, ?, ?)""", (nome, senha_hash, salt))
     CONN.commit()
 
-    print(f"Usuario {nome} cadastrado com sucesso!")
+    print(f"Usuário {nome} cadastrado com sucesso!")
 
-    
-# Verifica se o usuario é valido.
+# Verificando se o usuário é válido.
 def verifica_usuario(usuario, senha):
     CURSOR.execute("SELECT senha, salt FROM Usuarios WHERE usuario = ?", (usuario, ))
     resultado = CURSOR.fetchone()
@@ -51,27 +61,26 @@ def verifica_usuario(usuario, senha):
 
     return False
 
-
+# Definindo uma função para o menu principal.
 def menu():
     opc:str = ''
     while True:
-        system("clear || cls")
-        print("Insira seu usuario e sua senha")
-        user:str = input("Usuario: ")
-        senha:str = getpass.getpass("Senha: ")
+        system("clear || cls")  # Limpa a tela do terminal.
+        print("Insira seu usuário e sua senha")
+        user:str = input("Usuário: ")
+        senha:str = getpass.getpass("Senha: ")  # Solicita a senha de forma segura.
 
         if verifica_usuario(user, senha):
-            system("clear || cls")
-            print("Usuario Valido!")
+            system("clear || cls")  # Limpa a tela novamente.
+            print("Usuário válido!")
 
             input("\n\nDigite ENTER para continuar")
 
         else:
-            system("clear || cls")
-            print("Usuario invalido")
+            system("clear || cls")  # Limpa a tela em caso de usuário inválido.
+            print("Usuário inválido")
 
             input("\n\nDigite ENTER para continuar")
 
-
 if __name__ == "__main__":
-    menu()
+    menu()  # Inicia o menu quando o programa é executado diretamente.
